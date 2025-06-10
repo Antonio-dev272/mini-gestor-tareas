dc176c3ecb3c1800df2798bd9c9ba8458bfe8d3d
Antonio-dev272 <antonio.comba15@gmail.com>
2025-06-10 23:46:23 +0000

    feat: Configure initial GitHub Actions CI pipeline with tests and static analysis

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,27 @@
-from app import app  # Asegúrate que app.py tiene "app = Flask(__name__)"
+# tests/test_app.py
+import pytest
 
-def test_home_route():
-    tester = app.test_client()
-    response = tester.get('/')
+from app import app
+
+@pytest.fixture
+def client():
+    
+    app.config[TESTING] = True
+  
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client): 
+   
+    response = client.get('/')
+    assert response.status_code == 200 
+    assert b Mini Gestor de Tareas in response.data
+
+def test_task_creation_page(client):
+   
+  
+    response = client.get(new_task)
     assert response.status_code == 200
+   
+    assert b Crear Nueva Tarea in response.data
+
